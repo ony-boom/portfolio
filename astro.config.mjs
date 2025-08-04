4137319ba0506927b3597637b1b83d4b5aaec9e3
Ony <onyrakoto27@gmail.com>
2025-08-04 17:27:06 +0300

    removed vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
 
   markdown: {
     shikiConfig: {
