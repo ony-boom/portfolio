c0f2d63c208e00a04364d4e11feabb5188505df4
Ony <onyrakoto27@gmail.com>
2025-08-04 20:36:41 +0300

    added flake

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "My website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      pkgJson = builtins.fromJSON (builtins.readFile (self + "/package.json"));
+
+      pname = pkgJson.name;
+      version = pkgJson.version;
+      src = self;
+
+      pnpm = pkgs.pnpm_9;
+      node = pkgs.nodejs_latest;
+      makeWrapper = pkgs.makeWrapper;
+    in {
+      packages.default = pkgs.stdenv.mkDerivation {
+        inherit pname version src;
+
+        pnpmDeps = pnpm.fetchDeps {
+          inherit pname version src;
+          fetcherVersion = 1;
+          hash = "sha256-zIvEmxaSY+kOsqrL0UozKm18SrqEIYNw2xK7QU5vxyU=";
+        };
+
+        nativeBuildInputs = [
+          node
+          pnpm.configHook
+          makeWrapper
+        ];
+
+        buildInputs = [node];
+
+        buildPhase = ''
+          runHook preBuild
+          pnpm install --frozen-lockfile
+          pnpm build
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/lib $out/bin
+          cp -r dist/* $out/lib/
+
+          # Include runtime dependencies
+          cp -r node_modules $out/lib/
+
+          # Create a wrapper script
+          makeWrapper ${node}/bin/node $out/bin/ony-world \
+            --add-flags "$out/lib/server/entry.mjs" \
+            --set NODE_PATH "$out/lib/node_modules"
+
+          runHook postInstall
+        '';
+      };
+    });
+}
