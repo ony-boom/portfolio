b7cce5f181e31d06ec1cc720f00d9e316e1fea84
Ony <onyrakoto27@gmail.com>
2025-08-19 09:33:23 +0300

    rework with nextjs

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly PUBLIC_EMAILJS_PUB_KEY: string;
-  readonly PUBLIC_EMAILJS_SERVICE_ID: string;
-  readonly PUBLIC_EMAILJS_TEMPLATE_ID: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
