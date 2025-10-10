bf0524d6badda78038c0aee91bc550cbcd90ccd8
Ony <ony@ony.world>
2025-10-10 16:38:25 +0300

    empty blog state

--- apps/web/src/components/blog-list/normal-blog-list.tsx
+++ apps/web/src/components/blog-list/normal-blog-list.tsx
@@ -5,51 +5,55 @@ import { Blog } from "@/sanity/types";
 import { getLocale } from "next-intl/server";
 
 type BlogListItem = Pick<
-  Blog,
-  "_id" | "title" | "slug" | "_createdAt" | "publishedAt"
+	Blog,
+	"_id" | "title" | "slug" | "_createdAt" | "publishedAt"
 >;
 
 export async function NormalBlogList({ limit }: { limit?: number }) {
-  const locale = await getLocale();
-  const blogs = await client.fetch<BlogListItem[]>(
-    `*[_type == "blog" && language == $locale] | order(_createdAt asc){
+	const locale = await getLocale();
+	const blogs = await client.fetch<BlogListItem[]>(
+		`*[_type == "blog" && language == $locale] | order(_createdAt asc){
       _id,
       title,
       slug,
       publishedAt,
       _createdAt
     }${limit ? `[0...${limit}]` : ""}`,
-    { locale },
-  );
+		{ locale },
+	);
 
-  return (
-    <ul className="list-none space-y-2 p-0">
-      {blogs.map((blog, index) => {
-        const date = formatDateLong(blog.publishedAt, locale);
+	const isEmpty = blogs.length === 0;
 
-        return (
-          <li
-            className="text-muted-foreground animate-fade-in hover:text-foreground flex items-baseline justify-between gap-12 sm:gap-6 transition-colors duration-300"
-            key={blog._id}
-            style={{
-              animationDuration: "1s",
-              animationDelay: `${index * 0.2}s`,
-            }}
-          >
-            <Link
-              href={`/blog/${blog.slug}`}
-              title={blog.title}
-              className="truncate"
-            >
-              {blog.title}
-            </Link>
-            <span className="hidden flex-1 border-b border-dashed border-current sm:inline-block"></span>
-            <span className="flex-shrink-0 font-serif text-xs">
-              {date}
-            </span>
-          </li>
-        );
-      })}
-    </ul>
-  );
+	return (
+		<ul className="list-none space-y-2 p-0">
+			{isEmpty ? (
+				<p className="text-muted-foreground">¯\_(ツ)_/¯</p>
+			) : (
+				blogs.map((blog, index) => {
+					const date = formatDateLong(blog.publishedAt, locale);
+
+					return (
+						<li
+							className="text-muted-foreground animate-fade-in hover:text-foreground flex items-baseline justify-between gap-12 sm:gap-6 transition-colors duration-300"
+							key={blog._id}
+							style={{
+								animationDuration: "1s",
+								animationDelay: `${index * 0.2}s`,
+							}}
+						>
+							<Link
+								href={`/blog/${blog.slug}`}
+								title={blog.title}
+								className="truncate"
+							>
+								{blog.title}
+							</Link>
+							<span className="hidden flex-1 border-b border-dashed border-current sm:inline-block"></span>
+							<span className="flex-shrink-0 font-serif text-xs">{date}</span>
+						</li>
+					);
+				})
+			)}
+		</ul>
+	);
 }
